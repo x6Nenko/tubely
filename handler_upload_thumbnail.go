f53db91ba2f4b115c26bdcc4a097f90d5f7d676c
Max I. <clickkava@gmail.com>
2025-11-17 22:00:41 +0200

    Multipart Uploads

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -32,6 +33,58 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20  // 10 * 2^20 = 10 * 1024 * 1024 = 10MB
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+    respondWithError(w, http.StatusBadRequest, "Couldn't parse data", err)
+    return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, fileHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+    respondWithError(w, http.StatusBadRequest, "Couldn't get data", err)
+    return
+	}
+	defer file.Close()
+
+	mediaType := fileHeader.Header.Get("Content-Type")
+	if mediaType == "" {
+    respondWithError(w, http.StatusBadRequest, "Missing Content-Type for thumbnail", nil)
+    return
+	}
+
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+    respondWithError(w, http.StatusInternalServerError, "Couldn't get data", err)
+    return
+	}
+
+	metadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+    respondWithError(w, http.StatusInternalServerError, "Couldn't get metadata", err)
+    return
+	}
+
+	if metadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized", nil)
+    return
+	}
+
+	t := thumbnail{
+		data: imageData,
+		mediaType: mediaType,
+	}
+
+	videoThumbnails[videoID] = t
+
+	url := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID.String())
+	metadata.ThumbnailURL = &url
+	err = cfg.db.UpdateVideo(metadata)
+	if err != nil {
+		delete(videoThumbnails, videoID)
+    respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
+    return
+	}
+
+	respondWithJSON(w, http.StatusOK, metadata)
 }
